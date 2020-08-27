62f328704c33db747e6f9c038cfda8ca067380c1
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-08-27 13:55:00 +0100

    video5.7

--- public/cap-05/app/decorators/decorators.js
+++ public/cap-05/app/decorators/decorators.js
@@ -1,14 +1,18 @@
 export const logExecutionTime = (method, property, args) => {
+    console.log('start logExecutionTIme');
     console.time(property);
     const result = method(...args);
     console.timeEnd(property);
+    console.log('end logExecutionTime');
     return result;
 };
 
 export const inspectMethod = (method, property, args) => {
+    console.log('Start inspectMethod');
     console.log(`Método decorado: ${property}`);
     console.log(`Argumentos do método ${args}`);
     const result = method(...args);
     console.log(`resultado do método: ${result}`)
+    console.log('End inspectMethod');
     return result;
 };
--- public/cap-05/app/infra/decorate.js
+++ public/cap-05/app/infra/decorate.js
@@ -1,7 +1,7 @@
 export const decorate = (clazz, handler) => {
     Object.keys(handler).forEach(property => {
         const decorators = handler[property];
-        decorators.forEach(decorator => {
+        decorators.reverse().forEach(decorator => {
             const method = clazz.prototype[property];
             clazz.prototype[property] = function(...args) {
                 return decorator(method.bind(this), property, args);

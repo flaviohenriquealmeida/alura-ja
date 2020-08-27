c52a893230b701103b45e781c605aadc9446aeff
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-08-27 13:20:33 +0100

    video4.1

---
+++ public/cap-04/app/app.js
@@ -0,0 +1,8 @@
+
+function somaDoisNumeros(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+console.log(somaDoisNumeros(5, 5)); 
+console.log(somaDoisNumeros(10, 10));
+console.log(somaDoisNumeros(5, 5));

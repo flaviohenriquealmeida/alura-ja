08811ada67cd17fce026dd3f0ab19257e2d38f3b
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-08-27 13:28:09 +0100

    video4.4

--- public/cap-04/app/app.js
+++ public/cap-04/app/app.js
@@ -8,3 +8,15 @@ const somaDoisNumerosM = memoizer(somaDoisNumeros);
 console.log(somaDoisNumerosM(5, 5)); 
 console.log(somaDoisNumerosM(10, 10));
 console.log(somaDoisNumerosM(5, 5));
+
+const fatorial = memoizer(n => {
+    if (n <= 1) return 1;
+    return n * fatorial(--n);
+});
+
+const f1 = fatorial(4);
+const f2 = fatorial(3);
+const f3 = fatorial(2);
+console.log(f1);
+console.log(f2);
+console.log(f3);

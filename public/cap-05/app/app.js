e0504ee5ba4544f9f68dff1b1faa6aa9c6be707b
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-08-27 13:52:49 +0100

    video5.6

--- public/cap-05/app/app.js
+++ public/cap-05/app/app.js
@@ -1,14 +1,14 @@
 import { Person } from './models/person.js';
-import { logExecutionTime } from './decorators/decorators.js';
+import { logExecutionTime, inspectMethod } from './decorators/decorators.js';
 import { decorate } from './infra/decorate.js';
 decorate(
     Person, 
     {
-        speak: logExecutionTime,
-        getFullName: logExecutionTime
+        speak: [logExecutionTime, inspectMethod],
+        getFullName: [logExecutionTime]
     }
 );
 
 const person = new Person('Flávio', 'Almeida');
-person.getFullName();
+// person.getFullName();
 person.speak('Tudo bem?');
--- public/cap-05/app/decorators/decorators.js
+++ public/cap-05/app/decorators/decorators.js
@@ -1,9 +1,14 @@
 export const logExecutionTime = (method, property, args) => {
-    console.log(`Método decorado: ${property}`);
-    console.log(`Argumentos do método ${args}`);
     console.time(property);
     const result = method(...args);
     console.timeEnd(property);
+    return result;
+};
+
+export const inspectMethod = (method, property, args) => {
+    console.log(`Método decorado: ${property}`);
+    console.log(`Argumentos do método ${args}`);
+    const result = method(...args);
     console.log(`resultado do método: ${result}`)
     return result;
 };

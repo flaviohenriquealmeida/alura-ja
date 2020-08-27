6436e37ce9783c96455c4c9768dec6921b997b82
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-08-27 13:40:53 +0100

    video5.3

--- public/cap-05/app/app.js
+++ public/cap-05/app/app.js
@@ -1,5 +1,14 @@
 import { Person } from './models/person.js';
 
+const method = Person.prototype.speak;
+Person.prototype.speak = function(...args) {
+    console.log(`Argumentos do método: ${args}`);
+    console.time('speak');
+    const result = method.bind(this)(...args);
+    console.timeEnd('speak');
+    return result;
+
+};
 const person = new Person('Flávio', 'Almeida');
 person.getFullName();
 person.speak('Tudo bem?');

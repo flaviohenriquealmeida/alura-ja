959e30d1f9becb839bcf015a7733f41098446d98
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-08-27 13:25:17 +0100

    video4.3

--- public/cap-04/app/infra/memoizer.js
+++ public/cap-04/app/infra/memoizer.js
@@ -1,6 +1,16 @@
 export function memoizer(fn) {
+    const cache = new Map();
+    
     return (...args) => {
-        const result = fn(...args);
-        return result;
+        const key = JSON.stringify(args);
+        if (cache.has(key)) {
+            console.log(`Buscou do cache ${key}`);
+            return cache.get(key);
+        } else {
+            console.log(`Não encontrou no cache ${key}. Adicionando no cache`);
+            const result = fn(...args);
+            cache.set(key, result);
+            return result;
+        }
     }
 }

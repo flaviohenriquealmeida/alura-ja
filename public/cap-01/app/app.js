90198f80eda90b64aead6195f42a58ab3cb2292f
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-08-27 11:54:58 +0100

    video1.9

--- public/cap-01/app/app.js
+++ public/cap-01/app/app.js
@@ -1,4 +1,14 @@
 import { calculaImc } from './oms.js';
+import { Conta } from './models/conta.js';
 
 const imc = calculaImc({ peso: 68, altura: 1.71 });
 console.log(imc);
+
+const conta = new Conta({ 
+    titular: 'Flávio', 
+    banco: '123',
+    agencia: '456', 
+    numero: '789',
+});
+
+console.log(conta);
---
+++ public/cap-01/app/models/conta.js
@@ -0,0 +1,8 @@
+export class Conta {
+    constructor({ titular, banco, agencia, numero }) {
+      this.titular = titular;
+      this.banco = banco;
+      this.agencia = agencia;
+      this.numero = numero;
+    }  
+  }

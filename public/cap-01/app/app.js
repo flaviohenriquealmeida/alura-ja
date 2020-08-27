f052595b771168f69c8e8b6fbb4b2aa829d73b68
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-08-27 11:46:35 +0100

    video1.7

--- public/cap-01/app/app.js
+++ public/cap-01/app/app.js
@@ -1,4 +1,6 @@
 import { calculaImc } from './oms.js';
 
-const imc = calculaImc(1.71, 68);
+const imc = calculaImc({ altura: 1.71, peso: 68 });
+const imc2 = calculaImc({ peso: 68, altura: 1.71 });
 console.log(imc);
+console.log(imc2);
--- public/cap-01/app/oms.js
+++ public/cap-01/app/oms.js
@@ -1,3 +1,3 @@
-export function calculaImc(peso, altura) {
-    return peso / (altura * altura);
+export function calculaImc(param) {
+    return param.peso / (param.altura * param.altura);
 }
